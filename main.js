795db46a13b97fd51aa70f839ac67f562e9808ef
Chris Johnson <crjjrc@gmail.com>
2018-11-29 14:05:53 +1000

    Migrate cells on click.

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var resolution = 10;
 var svgNamespace = "http://www.w3.org/2000/svg";
 var cells = [];
 var operands = [0, 0];
-var cellOnColors = ['#FF6F5E', '#7380FF'];
-var cellOffColor = 'rgb(230, 230, 230)';
+var cellColors = ['rgb(230, 230, 230)', '#FF6F5E', '#7380FF'];
 var cellTextColor = 'rgb(100, 100, 100)';
 var guessBox = null;
 var root = null;
@@ -13,11 +12,14 @@ var fontSize;
 var integerPattern = /^\d+$/;
 var max = 100;
 var tailIndex = null;
+var isLocked = false;
+var overlay = null;
 
 function onReady() {
   var svg = document.getElementById('svg');
   root = document.getElementById('root');
   guessBox = document.getElementById('guessBox');
+  overlay = document.getElementById('overlay');
 
   cellSize = root.offsetWidth / resolution;
   fontSize = cellSize * 0.3;
@@ -39,8 +41,7 @@ function onReady() {
       cell.setAttribute('height', cellSize - 2 * padding);
       cell.setAttribute('rx', radius);
       cell.setAttribute('ry', radius);
-      cell.setAttribute('fill', cellOffColor);
-      cells.push(cell);
+      cells.push({svg: cell, state: 0});
       svg.appendChild(cell);
     }
   }
@@ -52,6 +53,14 @@ function onReady() {
   window.addEventListener('resize', resize);
   guessBox.addEventListener('blur', function() {
     guessBox.focus();
+  });
+
+  synchronizeCells();
+}
+
+function synchronizeCells() {
+  cells.forEach(cell => {
+    cell.svg.setAttribute('fill', cellColors[cell.state]);
   });
 }
 
@@ -113,6 +122,7 @@ function shakeGuess(guess) {
 }
 
 function next() {
+  isLocked = true;
   hidePrompt();
   erase();
 }
@@ -127,7 +137,8 @@ function generateNewOperands() {
   var bigger10 = roundUpTens(operands[0]);
   operands[1] = Math.floor((max - bigger10) * Math.random()) + 1;
 
-  // operands = [15, 75];
+  operands = [7, 88];
+  console.log("operands:", operands);
 }
 
 function hidePrompt() {
@@ -139,12 +150,9 @@ function erase() {
   // var bigger = Math.max(operands[0], operands[1]);
   // var i = 0;
   var nrows = roundUpTens(operands[0] + operands[1]) / 10;
-  console.log("nrows:", nrows);
   var r = 0;
   var soFar = operands[1];
-  console.log("soFar:", soFar);
   var task = setInterval(() => {
-    console.log("r:", r);
     if (r > nrows) {
       clearInterval(task);
       generateNewOperands();
@@ -153,7 +161,7 @@ function erase() {
     } else { 
       // Clear out old top row of operand 1.
       for (var i = 0; i < soFar % 10; ++i) {
-        cells[max - soFar + i].setAttribute('fill', cellOffColor);
+        cells[max - soFar + i].state = 0;
       }
 
       // Clear out prefix of new top row.
@@ -161,22 +169,21 @@ function erase() {
         soFar -= 10;
         var complement = 10 - soFar % 10;
         for (var i = 0; i < complement; ++i) {
-          cells[max - soFar - i - 1].setAttribute('fill', cellOffColor);
+          cells[max - soFar - i - 1].state = 0;
         }
       }
 
       // Clear out old top row of operand 0.
       if (tailIndex - operands[0] + 1 < max) {
         for (var i = 0; i < 10; ++i) {
-          // console.log("tailIndex - operands[0] + 1 + i:", tailIndex - operands[0] + 1 + i);
-          cells[tailIndex - operands[0] + 1 + i].setAttribute('fill', cellOffColor);
+          cells[tailIndex - operands[0] + 1 + i].state = 0;
         }
 
         // Fill in new bottom row of operand 0.
         tailIndex += 10;
         for (var i = 0; i < operands[0]; ++i) {
           if (tailIndex - i < max) {
-            cells[tailIndex - i].setAttribute('fill', cellOnColors[0]);
+            cells[tailIndex - i].state = 1;
           }
         }
       }
@@ -210,29 +217,133 @@ function isFringe(value, operandIndex) {
 
 function onClick(i) {
   return () => {
+    if (isLocked) {
+      return;
+    }
+
+    var r = Math.floor(i / 10);
+    var c = i % 10;
+
+    // if in first operand and there's room below
+    if (isDownwardlyMobile(cells[i], r, c)) {
+      cells[i].svg.style.cursor = 'default';
+      migrateCellDown(r, c);
+    } else if (isUpwardlyMobile(cells[i], r, c)) {
+      cells[i].svg.style.cursor = 'default';
+      migrateCellUp(r, c);
+    }
   }
+}
+
+function executeTasks(tasks, i = 0) {
+  if (i < tasks.length) {
+    setTimeout(() => {
+      tasks[i]();
+      synchronizeCells();
+      executeTasks(tasks, i + 1);
+    }, 50);
+  }
+}
+
+function migrateCellDown(r, c) {
+  isLocked = true;
+  overlay.style.display = 'block';
+  var tasks = [];
+
+  var dropClicked = () => {
+    cellAt(r + 1, c).state = 1;
+    cellAt(r, c).state = 1;
+    cellAt(r, (operands[0] - 1) % 10).state = 0;
+  };
+  tasks.push(dropClicked);
+
+  for (var cPrime = c + 1; cellAt(r + 1, cPrime).state == 0; ++cPrime) {
+    var shift = ((cPrime) => () => {
+      cellAt(r + 1, cPrime - 1).state = 0;
+      cellAt(r + 1, cPrime).state = 1;
+    })(cPrime);
+    tasks.push(shift);
+  }
+
+  var swapOperands = () => {
+    operands[0] -= 1;
+    operands[1] += 1;
+    cellAt(r + 1, cPrime - 1).state = 2;
+    tailIndex -= 1;
+    isLocked = false;
+    overlay.style.display = 'none';
+  };
+  tasks.push(swapOperands);
+
+  executeTasks(tasks);
+}
+
+function migrateCellUp(r, c) {
+  isLocked = true;
+  overlay.style.display = 'display';
+  var tasks = [];
+
+  var dropClicked = () => {
+    cellAt(r - 1, c).state = 2;
+    cellAt(r, c).state = 2;
+    cellAt(r, 9 - (operands[1] - 1) % 10).state = 0;
+  };
+  tasks.push(dropClicked);
+
+  for (var cPrime = c - 1; cellAt(r - 1, cPrime).state == 0; --cPrime) {
+    var shift = ((cPrime) => () => {
+      cellAt(r - 1, cPrime + 1).state = 0;
+      cellAt(r - 1, cPrime).state = 2;
+    })(cPrime);
+    tasks.push(shift);
+  }
+
+  var swapOperands = () => {
+    operands[1] -= 1;
+    operands[0] += 1;
+    cellAt(r - 1, cPrime + 1).state = 1;
+    tailIndex += 1;
+    isLocked = false;
+    overlay.style.display = 'none';
+  };
+  tasks.push(swapOperands);
+
+  executeTasks(tasks, 0);
+}
+
+function cellAt(r, c) {
+  return cells[r * 10 + c];
+}
+
+function isUpwardlyMobile(cell, r, c) {
+  return r > 0 && cell.state == 2 && cellAt(r - 1, c).state == 0 && cellAt(r - 1, 0).state == 1;
+}
+
+function isDownwardlyMobile(cell, r, c) {
+  return r < 9 && cell.state == 1 && cellAt(r + 1, c).state == 0 && cellAt(r + 1, 9).state == 2;
 }
 
 function onHover(i) {
   return () => {
-    var value = cellToValue(i);
-    var operandIndex = whichOperand(i);
-
-    // if in first operand and there's room below
-    if (operandIndex == 0 && isFringe(value, 0)) {
-      cells[i].style.cursor = 's-resize';
+    console.log("hi");
+    if (isLocked) {
+      return;
     }
 
-    // if in second operand and there's room above
-    if (operandIndex == 1 && isFringe(value, 1)) {
-      cells[i].style.cursor = 'n-resize';
+    var r = Math.floor(i / 10);
+    var c = i % 10;
+
+    if (isDownwardlyMobile(cells[i], r, c)) {
+      cells[i].svg.style.cursor = 's-resize';
+    } else if (isUpwardlyMobile(cells[i], r, c)) {
+      cells[i].svg.style.cursor = 'n-resize';
     }
   };
 }
 
 function onUnhover(i) {
   return () => {
-    cells[i].style.cursor = 'default'; 
+    cells[i].svg.style.cursor = 'default'; 
   };
 }
 
@@ -245,8 +356,9 @@ function fill() {
       setTimeout(drop, 200);
     } else {
       for (var i = 0; i < soFar; ++i) {
-        cells[max - 1 - i].setAttribute('fill', cellOnColors[1]);
+        cells[max - 1 - i].state = 2;
       }
+      synchronizeCells();
       soFar += 10;
     }
   }, 100);
@@ -269,13 +381,14 @@ function drop() {
       tailIndex += 10;
       for (var i = 0; i <= tailIndex; ++i) {
         if (whichOperand(i) < 0) {
-          cells[i].setAttribute('fill', cellOffColor);
+          cells[i].state = 0;
         } else {
-          cells[i].setAttribute('fill', cellOnColors[0]);
+          cells[i].state = 1;
         }
       }
+      synchronizeCells();
+      r += 1;
     }
-    r += 1;
   }, 100);
 }
 
@@ -284,6 +397,7 @@ function showPrompt() {
   guessBox.value = '';
   guessBox.style.display = 'block';
   guessBox.focus();
+  isLocked = false;
 }
 
 function lerp(currentMillis, targetMillis, oldValue, newValue) {
